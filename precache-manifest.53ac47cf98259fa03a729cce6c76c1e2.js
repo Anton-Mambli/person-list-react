02d1799eb7e8e9e1055b5117560a14be0a887bbf
= <=>
2020-04-17 12:27:14 +0300

    Updates

---
+++ precache-manifest.53ac47cf98259fa03a729cce6c76c1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7089883c65d7b861d092b83f3d546800",
+    "url": "/person-list-react/index.html"
+  },
+  {
+    "revision": "3a7ff51577414b38c116",
+    "url": "/person-list-react/static/css/main.9059d8f6.chunk.css"
+  },
+  {
+    "revision": "fd5eb58ed0b03e7f10a3",
+    "url": "/person-list-react/static/js/2.838bff2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/person-list-react/static/js/2.838bff2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7ff51577414b38c116",
+    "url": "/person-list-react/static/js/main.76f8d172.chunk.js"
+  },
+  {
+    "revision": "8825c80565c397ebfcd4",
+    "url": "/person-list-react/static/js/runtime-main.da66687c.js"
+  }
+]);
